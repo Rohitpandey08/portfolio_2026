50bfc2d7831b9453484a90e7dff17d613150ee56
Rohit Pandey <rohit.taurus08@gmail.com>
2026-01-31 01:58:15 +0530

    Change resume and rename to ROHIT PANDEY

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name's Pankaj Jain. I'm a Mern Stack developer based in Delhi, India.",
+    "My name's Rohit pandey. I'm a Data Analyst based in Delhi, India.",
   description2:
     "During the day I work as a developer and in the evening I help people on discord and also manage time to build my own projects. I spend my leisure hours writing code and exploring new technologies. I love to learn and explore new arenas.",
   image: 1,
